6cdb1fdfeeb523fd8e3974609b0e975788850b6c
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-07-01 15:13:14 +0200

    refactor: add get_temperature method and secure API key in environment variables

--- greetingserviceapp/views.py
+++ greetingserviceapp/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 import requests 
- 
+import os
+from dotenv import load_dotenv
+
 def get_location(ip):
     """
     fetch location of user IP address using ip-api.
@@ -16,6 +18,25 @@ def get_location(ip):
         return response.json().get('city', 'Unknown')
     except:
         return 'Unknown'
+    
+def get_temperature(city):
+    """
+    Get the temp of the location using OpenWeather API
+
+    Args:
+    city (str): location to fetch the temp.
+
+    Returns:
+    float: The temp or None if not found.
+    """
+    api_key = os.getenv('OPENWEATHER_API_KEY')
+    try:
+        response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
+        response.raise_for_status()
+        weather_data = response.json()
+        return weather_data['main'].get('temp')
+    except (requests.RequestException, KeyError):
+        return None
 
 def greet_visitor(request):
     """
@@ -30,6 +51,13 @@ def greet_visitor(request):
     visitor_name = request.GET.get('visitor_name', 'Visitor')
     client_ip = request.META.get('REMOTE_ADDR')
     location = get_location(client_ip)
+    temperature = get_temperature(location)
+
+    if temperature is not None:
+        greeting = f"Hello, {visitor_name}!, the temperature is {temperature:.2f} degrees Celsius in {location}"
+    else:
+        greeting = f"Hello, {visitor_name}!"
+
     response = {
         "client_ip": client_ip,
         "location": location,
--- greetingserviceproject/settings.py
+++ greetingserviceproject/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
