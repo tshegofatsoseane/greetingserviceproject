cce5cb915c77914981ede79436816dbedf838bc9
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-07-01 15:00:42 +0200

    update: add docstrings to functions

--- greetingserviceapp/views.py
+++ greetingserviceapp/views.py
@@ -2,6 +2,15 @@ from django.http import JsonResponse
 import requests 
  
 def get_location(ip):
+    """
+    fetch location of user IP address using ip-api.
+
+    Args:
+    ip (str): The IP address of the client.
+
+    Returns:
+    str: city matching the IP address or 'Unknown' if it's not found.
+    """
     try:
         response = requests.get(f'http://ip-api.com/json/{ip}')
         return response.json().get('city', 'Unknown')
@@ -9,6 +18,15 @@ def get_location(ip):
         return 'Unknown'
 
 def greet_visitor(request):
+    """
+    Greet visitor by his/her name and show IP address, location, and temperature of location.
+
+    Args:
+    request (HttpRequest): request object.
+
+    Returns:
+    JsonResponse: The greeting, the IP address & location, and the temperature as json response.
+    """
     visitor_name = request.GET.get('visitor_name', 'Visitor')
     client_ip = request.META.get('REMOTE_ADDR')
     location = get_location(client_ip)
